aef0c35c5f8d0311a9e7638cba6499d42ca37f54
khushboos <khushboo.singhvi@adyen.com>
2024-05-06 14:37:01 +0200

    Writing Unit test for HeaderDataBuilder.php

--- Observer/AddApplePayShortcuts.php
+++ Observer/AddApplePayShortcuts.php
@@ -16,6 +16,7 @@ use Adyen\ExpressCheckout\Model\Config\Source\ShortcutAreas;
 use Adyen\ExpressCheckout\Model\ConfigurationInterface;
 use Magento\Catalog\Block\ShortcutButtons;
 use Magento\Checkout\Block\QuoteShortcutButtons;
+use Magento\Checkout\Model\Session;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Exception\LocalizedException;
@@ -28,14 +29,22 @@ class AddApplePayShortcuts implements ObserverInterface
     private $configuration;
 
     /**
+     * @var Session
+     */
+    private $checkoutSession;
+
+    /**
      * AddApplePayShortcuts constructor
      *
      * @param ConfigurationInterface $configuration
+     * @param Session $checkoutSession
      */
     public function __construct(
-        ConfigurationInterface $configuration
+        ConfigurationInterface $configuration,
+        Session $checkoutSession
     ) {
         $this->configuration = $configuration;
+        $this->checkoutSession = $checkoutSession;
     }
 
     /**
@@ -48,12 +57,22 @@ class AddApplePayShortcuts implements ObserverInterface
     public function execute(Observer $observer)
     {
         $currentPageIdentifier = $this->getCurrentPageIdentifier($observer);
-        if (!in_array(
-            $currentPageIdentifier,
-            $this->configuration->getShowApplePayOn()
-        )) {
+
+        //Testing
+        $adyenPaymentMethods = $this->checkoutSession->getData('adyenPaymentMethods');
+
+        /*
+         * Not in PDP/Cart/Minicart
+         * And Not coming from /paymentMethods Response
+         */
+        if (
+            !in_array($currentPageIdentifier, $this->configuration->getShowApplePayOn())
+            && isset($adyenPaymentMethods['paymentMethodsResponse'])
+            && !in_array('ApplePay', array_column($adyenPaymentMethods['paymentMethodsResponse']['paymentMethods'], 'name'))
+        ) {
             return;
         }
+
         /** @var ShortcutButtons $shortcutButtons */
         $shortcutButtons = $observer->getEvent()->getContainer();
         $shortcut = $shortcutButtons->getLayout()->createBlock(Button::class);
